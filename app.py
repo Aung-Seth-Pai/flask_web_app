562f1033cd9499dea79a2e2b0c2a0267af9a5c3d
Zephael Lin <Zephael Lin>
2019-08-02 00:43:01 +0630

    project begin!

---
+++ app.py
@@ -0,0 +1,25 @@
+'''
+	Webapp for creating flexible schedule
+	Google Calendar, Facebook Events, Gmail APIs
+	Data Analysis on Task Accomplishments
+'''
+
+from flask import Flask, redirect, render_template
+from flask_wtf import FlaskForm
+import sqlite3
+
+# create flask server
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# create database for users, todolist, time
+
+# Homepage
+@app.route("/")
+@app.route("/home")
+def index():
+	return render_template("index.html", title="Home")
+
+# enable server setup with (python filename.py) without (flask run)
+if __name__ == '__main__':
+	app.run(debug=True)
