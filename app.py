6102156b70d7bdf49c9d46296d7380a1253e62d1
Zephael Lin <Zephael Lin>
2019-08-16 16:54:06 +0630

    initial navbar design and signup form

--- app.py
+++ app.py
@@ -4,12 +4,13 @@
 	Data Analysis on Task Accomplishments
 '''
 
-from flask import Flask, redirect, url_for, render_template
-from flask_wtf import FlaskForm
+from flask import Flask, redirect, url_for, render_template, request
+from form import RegisterForm
 import sqlite3
 
 # create flask server
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "9dfc64c5f1013516999739f20a4b13bc"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # create database for users, todolist, time
@@ -19,6 +20,14 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/home")
 def index():
 	return render_template("index.html", title="Home")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegisterForm()
+	if form.validate_on_submit():
+		return redirect(url_for('index'))
+	return render_template("register.html", title="register", form=form)
+
 
 # enable server setup with (python filename.py) without (flask run)
 if __name__ == '__main__':
