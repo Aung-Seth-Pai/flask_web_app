ac49bc851c956cf827208d3fc419d574f034f444
Zephael Lin <Zephael Lin>
2019-10-09 19:17:04 +0630

    add a readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spark
+	This little flask web-app is being created with no particular orientation in mind. Maybe I'll use this later for deploying my ML models which I am procrastinating on. 
--- run_app.bat
+++
@@ -1,9 +0,0 @@
-rem "start virtual env and web server"
-rem "/k = keep screen on"
-start cmd.exe /k "color b && D:\Workspace\flextime\spark\scripts\activate.bat && D: && cd/Workspace/flextime && python spark.py"
-
-rem "go to C drive"
-C:
-
-rem "locate and run chrome"
-start "chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window "http://localhost:5000"
